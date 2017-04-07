2a4555c0683ca134d753a3098c0c3d1b53ac8c2b
castusinfo <udaykatikala@gmail.com>
2017-04-07 17:15:53 +0530

    save date time done changes

--- AdminCreateService/src/com/digitalhealthcare/DigihealthCareAdminCreateServiceBL.java
+++ AdminCreateService/src/com/digitalhealthcare/DigihealthCareAdminCreateServiceBL.java
@@ -15,6 +15,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cis.CISConstants;
 import com.cis.CISResults;
+import com.cis.TimeCheck;
 import com.cis.testServiceTime;
 
 public class DigihealthCareAdminCreateServiceBL {
@@ -27,26 +28,10 @@ public class DigihealthCareAdminCreateServiceBL {
 		Logger logger = Logger.getLogger(DigihealthCareAdminCreateServiceBL.class);
 		
 		// Capture service Start time
-		 testServiceTime seriveTimeCheck=new testServiceTime();
-		 Calendar current = Calendar.getInstance();
-	      DateFormat formatterTime = new SimpleDateFormat(CISConstants.DATE_FORMAT);
-	      TimeZone objTime = TimeZone.getTimeZone(CISConstants.TIME_ZONE);
-	      formatterTime.setTimeZone( objTime);
-	    //  System.out.println("Local:: " +current.getTime());
-	    //  System.out.println("CST:: "+ formatterTime.format(current.getTime()));
-		  String serviceStartTime=formatterTime.format(current.getTime());
-		  Calendar currentdate = Calendar.getInstance();
-	      DateFormat formatter = new SimpleDateFormat(CISConstants.GS_DATE_FORMAT);
-	      TimeZone obj = TimeZone.getTimeZone(CISConstants.TIME_ZONE);
-	      formatter.setTimeZone(obj);
-	      String saveDate=formatter.format(currentdate.getTime());
+		  TimeCheck time=new TimeCheck();
+	      String saveDate=time.getTimeZone();
 	      String careTakerphone="";
 	      String sessionId = UUID.randomUUID().toString();
-	     /* String sessionId = UUID.randomUUID().toString();
-		  String deleteInd=CISConstants.DELETE_IND;
-		  String userId=DigestUtils.sha1Hex(sessionId);*/
-	      /*Random rnd = new Random();
-	      int userId = 100000 + rnd.nextInt(900000);*/
 	      String userId=DigestUtils.sha1Hex(sessionId);
 	      String upToNCharacters = userId.substring(0, Math.min(userId.length(), 8));
 	      userId=upToNCharacters;
@@ -58,22 +43,6 @@ public class DigihealthCareAdminCreateServiceBL {
 		  {
 	         cisResult = adminCreateServiceDAO.addPatients(savePatient.getAppId(),userId,savePatient.getAccountType(),savePatient.getFirstName(),savePatient.getLastName(),phoneNumber,savePatient.getPassword(),savePatient.getEmailId(),savePatient.getGender(),savePatient.getPhoto(),savePatient.getDob(),saveDate,sessionId);
 		  }
-
-	      //Capture Service End time
-
-	   // Capture Service End time
-
-		  Calendar ServiceEnd= Calendar.getInstance();
-	      DateFormat formatter1 = new SimpleDateFormat(CISConstants.DATE_FORMAT);
-	      TimeZone obj1 = TimeZone.getTimeZone(CISConstants.TIME_ZONE);
-	      formatter1.setTimeZone(obj1);
-		  String serviceEndTime=formatter1.format(ServiceEnd.getTime());
-		  long result=seriveTimeCheck.getServiceTime(serviceEndTime,serviceStartTime);
-
-		  logger.info("Admin update profile query time:: " +result);
-
-		  logger.info("Database time for Add patients service:: " +result);
-
 		return cisResult;
 		
 	}
